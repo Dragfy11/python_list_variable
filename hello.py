1bcfaeed4f2e3c4636a6ae33e0f3a739a7b388fd
Dragfy11 <dragfy_dark@hotmail.com>
2021-04-19 12:40:01 +0200

    list + variable

---
+++ hello.py
@@ -0,0 +1,69 @@
+# message = "hello"
+
+# print(message)
+
+# x = 14
+
+# y = 2
+
+# print(int(x/y))
+
+#list
+
+# my_list = ["Batgirl", "Batman", "Supergirl", "Superman", "Aquaman"]
+
+
+###### - ajouter un élément ####
+
+# my_list.append("Harley Quinn")
+
+# my_list.append("Ivy")
+
+
+##### - supprimer un élément #####
+
+# del my_list[4]
+
+##### - sauvegarder un élément en la retirant #####
+
+# my_variable = my_list.pop()
+
+
+###### - retirer un élément par son nom #####
+
+
+# my_list.remove("Batman")
+
+
+##### - trier tout les éléments #####
+
+# my_list.sort()
+
+
+#### - voir la longeur de la list  #####
+
+# print(len(my_list))
+
+# print(my_list)
+
+
+###### - voir l'élément sauvegarder ######
+
+# print(my_variable)
+
+
+my_list = ["Batgirl", "Batman", "Supergirl", "Superman", "Aquaman"]
+
+my_int = [2, 3, 4, 5, 3, 2, 4, 5]
+
+# print(my_list)
+
+##### - cité tout les éléments#####
+
+# for superhero in my_int:
+#     print(superhero*10)
+
+for i in range(2,6):
+    print(my_int[i])
+
+
